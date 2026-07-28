363d749c81b9d84da45e1a38219dac0a153585d9
Vladislav Botvin <darkvlados@gmail.com>
2026-07-28 18:36:35 +0300

    Delta Chat -> Delta Chat Ygg

--- src/main/java/org/thoughtcrime/securesms/connect/DcHelper.java
+++ src/main/java/org/thoughtcrime/securesms/connect/DcHelper.java
@@ -543,7 +543,7 @@ public class DcHelper {
       if (lastError != null && !lastError.isEmpty()) {
         Log.w(TAG, "Opening account failed, trying to share error: " + lastError);
 
-        String subject = "Delta Chat failed to update";
+        String subject = "Delta Chat Ygg failed to update";
         String email = "delta@merlinux.eu";
 
         new AlertDialog.Builder(context)
--- src/main/java/org/thoughtcrime/securesms/notifications/NotificationCenter.java
+++ src/main/java/org/thoughtcrime/securesms/notifications/NotificationCenter.java
@@ -880,7 +880,7 @@ public class NotificationCenter {
                   .setSmallIcon(R.drawable.icon_notification)
                   .setColor(context.getResources().getColor(R.color.delta_primary, null))
                   .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                  .setContentTitle("Delta Chat")
+                  .setContentTitle("Delta Chat Ygg")
                   .setContentText("New messages")
                   .setContentIntent(getOpenChatlistIntent(accountId));
           if (privacy.isDisplayContact() && !TextUtils.isEmpty(accountTag)) {
